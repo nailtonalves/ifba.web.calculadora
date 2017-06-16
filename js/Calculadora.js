a46cbd482107652f4abe90dbffc82045fd7256ac
Airton Ribeiro <niwdm@yahoo.com.br>
2017-06-16 00:31:59 -0300

    JS bt de 0 a 1
    JS bt de 0 a 1 e HTML

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -6,7 +6,7 @@ var pvisor = document.getElementById("visor");
 function operar(num, operador) {
 	num1 = parseInt(num);
 	operacao = parseInt(operador);
-	pvisor.value = "Digite o segundo número";
+	pvisor.value = "Digite o segundo numero";
 }
 
 function executar(num) {
@@ -39,9 +39,81 @@ function divi() {
 //Funcoes dos botoes
 
 function bt1(num) {
-	if ( (num == "resultado") || (num == "Digite o segundo número")) {
-		pvisor.value = 1;
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "1";
 	} else {
-		pvisor.value = num + "1";
+		document.getElementById("visor").value = num + "1";
+	}
+}
+
+function bt2(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "2";
+	} else {
+		document.getElementById("visor").value = num + "2";
+	}
+}
+
+function bt3(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "3";
+	} else {
+		document.getElementById("visor").value = num + "3";
+	}
+}
+
+function bt4(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "4";
+	} else {
+		document.getElementById("visor").value = num + "4";
+	}
+}
+
+function bt5(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "5";
+	} else {
+		document.getElementById("visor").value = num + "5";
+	}
+}
+
+function bt6(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "6";
+	} else {
+		document.getElementById("visor").value = num + "6";
+	}
+}
+
+function bt7(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "7";
+	} else {
+		document.getElementById("visor").value = num + "7";
+	}
+}
+
+function bt8(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "8";
+	} else {
+		document.getElementById("visor").value = num + "8";
+	}
+}
+
+function bt9(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero")) {
+		document.getElementById("visor").value = "9";
+	} else {
+		document.getElementById("visor").value = num + "9";
+	}
+}
+
+function bt0(num) {
+	if ( (num == "resultado") || (num == "Digite o segundo numero") || (num == "0") ) {
+		document.getElementById("visor").value = "0";
+	} else {
+		document.getElementById("visor").value = num + "0";
 	}
 }
